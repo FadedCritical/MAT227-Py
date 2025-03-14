950cdaff40971e1763cf0b7b8962a8d196c9a1de
FadedCritical <ronald.armisteadtyler@gmail.com>
2025-03-13 20:27:23 -0700

    Added SAS

---
+++ SAS.py
@@ -0,0 +1,22 @@
+#Created by
+#Ronald Armistead-Tyler
+
+#SAS Formulas:
+#{Law of Cosines}
+#a^2 = b^2 + C^2 - 2bc * cos(A)
+#{Law of Sines}
+#arcsin(sin(A) * b / a)
+
+from math import sqrt, sin, cos, degrees, radians, asin
+
+angleA = radians(int(input("Angle A: ")))
+sideB = float(input("Side B: "))
+sideC = float(input("Side C: "))
+
+sideA = sqrt(pow(sideB, 2) + pow(sideC, 2) - 2 * sideB * sideC * cos(angleA))
+angleB = degrees(asin(sin(angleA) * sideB / sideA))
+angleC = degrees(asin(sin(angleA) * sideC / sideA))
+
+print("\n\nSide A:", round(sideA, 3))
+print("Angle B:", str(round(angleB, 3)) + "°")
+print("Angle C:", str(round(angleC, 3)) + "°")
