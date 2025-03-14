93daab39422021c4d634594ef0879825d31d309d
FadedCritical <ronald.armisteadtyler@gmail.com>
2025-03-13 22:08:09 -0700

    Changed PIE

--- PIE.py
+++ PIE.py
@@ -1,5 +1,7 @@
 #Created by
 #Ronald Armistead-Tyler
+from math import floor
+
 def fact(num):
     if(num==1 or num == 0):
         return 1
@@ -16,11 +18,20 @@ b = int(input("Bars: "))
 e = int(input("Item Limit: ")) + 1
 
 output = comb(s + b, b)
+if e - 1 > 0 : 
+    for i in range(e):
+        if(i % 2 == 0):
+            output -= comb(b + 1, i + 1) * comb((s + b) - (e * (i + 1)), b)
+        else:
+            output += comb(b + 1, i + 1) * comb((s + b) - (e * (i + 1)), b)
+elif e - 1 == 0:
+    bins = int(input("Bins: "))
+    binVolume = int(input("Items per bin: "))
+    if bins > 0:
+        for i in range(floor((s + b) / bins) - 1):
+            if(i % 2 == 0):
+                output -= comb(b + 1, i + 1) * comb((s + b) - ((binVolume + 1) * (i + 1)), b)
+            else:
+                output += comb(b + 1, i + 1) * comb((s + b) - ((binVolume + 1) * (i + 1)), b)
 
-for i in range(e):
-    if(i % 2 == 0):
-        output -= comb(b + 1, i + 1) * comb((s + b) - (e * (i + 1)), b)
-    else:
-        output += comb(b + 1, i + 1) * comb((s + b) - (e * (i + 1)), b)
-
-print(output)
+print(round(output))
